aab7cc338705feb8e2ec421e482d450fb348aca2
razansu23-meet <109904088+razansu23-meet@users.noreply.github.com>
2025-10-08 14:31:00 +0300

    hw3

--- HW2/src/ArrayUtils.java
+++ HW2/src/ArrayUtils.java
@@ -36,8 +36,50 @@ public class ArrayUtils {
         return array;
     }
 
-    public static int findShortestPath(int[][] m, int i, int j) {
-      
-    }
+    private static int findShortestPathRec(int[][] m, int i, int j, HashSet<Integer> notVisited, int indention) {
+//		System.out.println("  ".repeat(indention) + "performing " + i + " -> " + j + " on notVisited=" + notVisited);
+		if (i == j) {
+			return 0;
+		} else if (indention > 10){
+			return 0;
+		}
+
+		int n = m.length;
+
+		ArrayList<Integer> toVisit = new ArrayList<>();
+		for (int index = 0; index < n; index++) {
+			int i1 = m[i][index];
+//			System.out.println("  ".repeat(indention) + " check inx " + index + " got " + i1);
+			if (notVisited.contains(index) && i1 == 1) {
+				toVisit.add(index);
+			}
+		}
+//		System.out.println("constructed " + toVisit);
+
+		int latgestPossibleValue = (int) Math.pow(n, 2) + 1;
+		int min = latgestPossibleValue;
+		for (int node : toVisit) {
+			HashSet newNotVisited = new HashSet<>(notVisited);
+			newNotVisited.remove(node);
+			int shortestPath = findShortestPathRec(m, node, j, newNotVisited, indention + 1);
+			if (shortestPath != -1) {
+				min = Math.min(shortestPath, min);
+			}
+		}
+		if (min == latgestPossibleValue) {
+			return -1;
+		}
+
+		return min + 1;
+	}
+
+	public static int findShortestPath(int[][] m, int i, int j) {
+		HashSet<Integer> notVisited = new HashSet<>();
+		for (int index = 0; index < m.length; index++) {
+			notVisited.add(index);
+		}
+		return findShortestPathRec(m, i, j, notVisited, 0);
+	}
+
 
 }
--- HW2/src/StringUtils.java
+++ HW2/src/StringUtils.java
@@ -1,4 +1,71 @@
+import java.util.Arrays;
 
 public class StringUtils {
-	public static String findSortedSequence (String str)
+	public static void main(String[] args) {
+		
+	}
+	public static String findSortedSequence(String str) {
+	    int n = str.length();
+	    int i = 0;
+
+	    String max_str = "";
+	    int max_len = 0;
+
+	    String curr = "";
+	    int curr_len = 0;
+
+	    String prevWord = null;
+
+	    while (i < n) {
+	        String word = "";
+	        while (i < n && str.charAt(i) != ' ') {
+	            word += str.charAt(i);
+	            i++;
+	        }
+	        if (i < n && str.charAt(i) == ' ') i++;
+
+	        if (prevWord == null || word.compareTo(prevWord) >= 0) {
+	            if (curr_len > 0) curr += " ";
+	            curr += word;
+	            curr_len++;
+	        } else {
+	            if (curr_len > max_len || (curr_len == max_len && curr_len > 0)) {
+	                max_len = curr_len;
+	                max_str = curr;
+	            }
+	            curr = word;
+	            curr_len = 1;
+	        }
+	        prevWord = word;
+	    }
+
+	    if (curr_len > max_len || (curr_len == max_len && curr_len > 0)) {
+	        max_len = curr_len;
+	        max_str = curr;
+	    }
+
+	    if (max_len < 2) return "";
+	    return max_str;
+	}
+	public static boolean isEditDistanceOne(String a, String b) {
+	    int n = a.length(), m = b.length();
+	    if (n - m > 1 || m - n > 1) return false;
+
+	    int i = 0, j = 0, edits = 0;
+	    while (i < n && j < m) {
+	        char ca = a.charAt(i), cb = b.charAt(j);
+	        if (ca == cb) { i++; j++; }
+	        else {
+	            if (++edits > 1) return false;
+	            if (n > m) i++;
+	            else if (m > n) j++;
+	            else { i++; j++; }
+	        }
+	    }
+	    if (i < n || j < m) edits++;
+	    return edits == 1;
+	}
+
+		
+	
 }
